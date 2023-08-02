942a7fc7a2fa9c8676fa93e45e58bb7f3aa3d5f4
Sebasalazaro <sebasalazaro@gmail.com>
2023-08-02 11:45:20 -0500

    URL de About y cambio de color

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -19,7 +19,9 @@ from django.urls import path
 from movie import views as movieViews
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
+    path('', movieViews.about),
 ]
