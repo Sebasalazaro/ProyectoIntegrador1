fba2f97f31ff58b1aa0643a2eb4ea433237d3e81
Sebasalazaro <sebasalazaro@gmail.com>
2024-02-07 10:12:24 -0500

    Cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html' , {'name':'Sebastian Salazar'})
+    return render(request, 'home.html' , {'name':'Sebastian Salazar Osorio'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
