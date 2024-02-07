cee094b98fdaf0456076d68fe3313e99395e86ff
Sebasalazaro <sebasalazaro@gmail.com>
2024-02-07 11:25:24 -0500

    Visualización de películas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains = searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm , 'movies':movies}, )
+    
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html' , {'name':'Sebastian Salazar Osorio'})
+    #return render(request, 'home.html' , {'name':'Sebastian Salazar Osorio'})
+
+    
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
